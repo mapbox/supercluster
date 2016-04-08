37de2deb7f21a98aad30ed1f75c0c20626c4dfc8
Vladimir Agafonkin <agafonkin@gmail.com>
2016-04-08 16:41:04 -0700

    switch to kdbush for indexing points

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var rbush = require('rbush');
+var kdbush = require('kdbush');
 
 module.exports = supercluster;
 
@@ -10,7 +10,7 @@ function supercluster(options) {
 
 function SuperCluster(options) {
     this.options = extend(Object.create(this.options), options);
-    this._initTrees();
+    this.trees = new Array(this.options.maxZoom + 1);
 }
 
 SuperCluster.prototype = {
@@ -19,7 +19,7 @@ SuperCluster.prototype = {
         maxZoom: 16,  // max zoom level to cluster the points on
         radius: 40,   // cluster radius in pixels
         extent: 512,  // tile extent (radius is calculated relative to it)
-        nodeSize: 16, // size of the R-tree leaf node, affects performance
+        nodeSize: 64, // size of the KD-tree leaf node, affects performance
         log: false    // whether to log timing info
     },
 
@@ -31,6 +31,8 @@ SuperCluster.prototype = {
         var timerId = 'prepare ' + points.length + ' points';
         if (log) console.time(timerId);
 
+        this.points = points;
+
         // generate a cluster object for each point
         var clusters = points.map(createPointCluster);
         if (log) console.timeEnd(timerId);
@@ -40,12 +42,16 @@ SuperCluster.prototype = {
         for (var z = this.options.maxZoom; z >= this.options.minZoom; z--) {
             var now = +Date.now();
 
-            this.trees[z + 1].load(clusters); // index input points into an R-tree
+            // index input points into a KD-tree
+            this.trees[z + 1] = kdbush(clusters, getX, getY, this.options.nodeSize, Float32Array);
+
             clusters = this._cluster(clusters, z); // create a new set of clusters for the zoom
 
             if (log) console.log('z%d: %d clusters in %dms', z, clusters.length, +Date.now() - now);
         }
-        this.trees[this.options.minZoom].load(clusters); // index top-level clusters
+
+        // index top-level clusters
+        this.trees[this.options.minZoom] = kdbush(clusters, getX, getY, this.options.nodeSize, Float32Array);
 
         if (log) console.timeEnd('total time');
 
@@ -53,27 +59,33 @@ SuperCluster.prototype = {
     },
 
     getClusters: function (bbox, zoom) {
-        var projBBox = [lngX(bbox[0]), latY(bbox[3]), lngX(bbox[2]), latY(bbox[1])];
-        var clusters = this.trees[this._limitZoom(zoom)].search(projBBox);
-        return clusters.map(getClusterJSON);
+        var tree = this.trees[this._limitZoom(zoom)];
+        var ids = tree.range(lngX(bbox[0]), latY(bbox[3]), lngX(bbox[2]), latY(bbox[1]));
+        var clusters = [];
+        for (var i = 0; i < ids.length; i++) {
+            clusters.push(getClusterJSON(tree.points[ids[i]], this.points));
+        }
+        return clusters;
     },
 
     getTile: function (z, x, y) {
         var z2 = Math.pow(2, z);
         var extent = this.options.extent;
         var p = this.options.radius / extent;
-        var clusters = this.trees[this._limitZoom(z)].search([
+        var tree = this.trees[this._limitZoom(z)];
+        var ids = tree.range(
             (x - p) / z2,
             (y - p) / z2,
             (x + 1 + p) / z2,
-            (y + 1 + p) / z2
-        ]);
-        if (!clusters.length) return null;
+            (y + 1 + p) / z2);
+
+        if (!ids.length) return null;
+
         var tile = {
             features: []
         };
-        for (var i = 0; i < clusters.length; i++) {
-            var c = clusters[i];
+        for (var i = 0; i < ids.length; i++) {
+            var c = tree.points[ids[i]];
             var feature = {
                 type: 1,
                 geometry: [[
@@ -91,17 +103,6 @@ SuperCluster.prototype = {
         return Math.max(this.options.minZoom, Math.min(z, this.options.maxZoom + 1));
     },
 
-    _initTrees: function () {
-        this.trees = [];
-        // make an R-Tree index for each zoom level
-        for (var z = 0; z <= this.options.maxZoom + 1; z++) {
-            this.trees[z] = rbush(this.options.nodeSize);
-            this.trees[z].toBBox = toBBox;
-            this.trees[z].compareMinX = compareMinX;
-            this.trees[z].compareMinY = compareMinY;
-        }
-    },
-
     _cluster: function (points, zoom) {
         var clusters = [];
         var r = this.options.radius / (this.options.extent * Math.pow(2, zoom));
@@ -114,22 +115,19 @@ SuperCluster.prototype = {
             if (p.zoom <= zoom) continue;
             p.zoom = zoom;
 
-            // find all nearby points with a bbox search
-            bbox[0] = p.wx - r;
-            bbox[1] = p.wy - r;
-            bbox[2] = p.wx + r;
-            bbox[3] = p.wy + r;
-            var bboxNeighbors = this.trees[zoom + 1].search(bbox);
+            // find all nearby points
+            var tree = this.trees[zoom + 1];
+            var neighborIds = tree.within(p.wx, p.wy, r);
 
             var foundNeighbors = false;
             var numPoints = p.numPoints;
             var wx = p.wx * numPoints;
             var wy = p.wy * numPoints;
 
-            for (var j = 0; j < bboxNeighbors.length; j++) {
-                var b = bboxNeighbors[j];
+            for (var j = 0; j < neighborIds.length; j++) {
+                var b = tree.points[neighborIds[j]];
                 // filter out neighbors that are too far or already processed
-                if (zoom < b.zoom && distSq(p, b) <= r * r) {
+                if (zoom < b.zoom) {
                     foundNeighbors = true;
                     b.zoom = zoom; // save the zoom (so it doesn't get processed twice)
                     wx += b.wx * b.numPoints; // accumulate coordinates for calculating weighted center
@@ -158,16 +156,6 @@ SuperCluster.prototype = {
     }
 };
 
-function toBBox(p) {
-    return [p.x, p.y, p.x, p.y];
-}
-function compareMinX(a, b) {
-    return a.x - b.x;
-}
-function compareMinY(a, b) {
-    return a.y - b.y;
-}
-
 function createCluster(x, y) {
     return {
         x: x, // cluster center
@@ -175,20 +163,20 @@ function createCluster(x, y) {
         wx: x, // weighted cluster center
         wy: y,
         zoom: Infinity, // the last zoom the cluster was processed at
-        point: null,
+        id: -1,
         numPoints: 1
     };
 }
 
-function createPointCluster(p) {
+function createPointCluster(p, i) {
     var coords = p.geometry.coordinates;
     var cluster = createCluster(lngX(coords[0]), latY(coords[1]));
-    cluster.point = p;
+    cluster.id = i;
     return cluster;
 }
 
-function getClusterJSON(cluster) {
-    return cluster.point ? cluster.point : {
+function getClusterJSON(cluster, points) {
+    return cluster.id !== -1 ? points[cluster.id] : {
         type: 'Feature',
         properties: getClusterProperties(cluster),
         geometry: {
@@ -229,14 +217,14 @@ function yLat(y) {
     return 360 * Math.atan(Math.exp(y2)) / Math.PI - 90;
 }
 
-// squared distance between two points
-function distSq(a, b) {
-    var dx = a.wx - b.wx;
-    var dy = a.wy - b.wy;
-    return dx * dx + dy * dy;
-}
-
 function extend(dest, src) {
     for (var id in src) dest[id] = src[id];
     return dest;
+}
+
+function getX(p) {
+    return p.wx;
+}
+function getY(p) {
+    return p.wy;
 }
