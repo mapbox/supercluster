48760d91d9433bbdf0d9f2438a88a5af54f3a1e7
Vladimir Agafonkin <agafonkin@gmail.com>
2016-07-21 19:28:35 +0300

    fix tile clusters around dateline

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ var placesTile = require('./fixtures/places-z0-0-0.json');
 
 test(function (t) {
     var index = supercluster().load(places.features);
-    t.same(index.getTile(0, 0, 0), placesTile);
+    var tile = index.getTile(0, 0, 0);
+    t.same(tile.features, placesTile.features);
     t.end();
 });
