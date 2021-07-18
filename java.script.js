84b7a0e84efa63d0baf9cf553ccedd2a12e96a17
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-07-18 13:46:54 -0500

    Update java.script.js

--- java.script.js
+++ java.script.js
@@ -1,1 +1,7 @@
-'<script src="https://unpkg.com/supercluster@7.1.2/dist/supercluster.min.js"></script>
+' script src="https://unpkg.com/supercluster@7.1.2/dist/supercluster.min.js"></script>
+const index = new Supercluster({
+    radius: 40,
+    maxZoom: 16
+});
+index.load(points);
+index.getClusters([-180, -85, 180, 85], 2);
