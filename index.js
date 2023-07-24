14375bea7e0583af1736c2c3e97bd6ce531eae33
mrkwanzaa <sledgeguter@gmail.com>
2023-07-24 15:51:13 -0400

    Switch to structuredClone for deep cloning

--- index.js
+++ index.js
@@ -369,11 +369,11 @@ export default class Supercluster {
     _map(data, i, clone) {
         if (data[i + OFFSET_NUM] > 1) {
             const props = this.clusterProps[data[i + OFFSET_PROP]];
-            return clone ? _clone(props, [], [], true) : props;
+            return clone ? structuredClone(props) : props;
         }
         const original = this.points[data[i + OFFSET_ID]].properties;
         const result = this.options.map(original);
-        return clone && result === original ? _clone(result, [], [], true) : result;
+        return clone && result === original ? structuredClone(result) : result;
     }
 }
 
@@ -423,70 +423,3 @@ function yLat(y) {
     return 360 * Math.atan(Math.exp(y2)) / Math.PI - 90;
 }
 
-// clone reg exp
-function _cloneRegExp(pattern) {
-    return new RegExp(
-        pattern.source,
-        (pattern.global ? 'g' : '') +
-          (pattern.ignoreCase ? 'i' : '') +
-          (pattern.multiline ? 'm' : '') +
-          (pattern.sticky ? 'y' : '') +
-          (pattern.unicode ? 'u' : '')
-    );
-}
-// Find object type
-function type(val) {
-    return val === null ?
-        'Null' :
-        val === undefined ?
-            'Undefined' :
-            Object.prototype.toString.call(val).slice(8, -1);
-}
-
-// deep clone
-function _clone(value, refFrom, refTo, deep) {
-    const copy = function copy(copiedValue) {
-        const len = refFrom.length;
-        let idx = 0;
-        while (idx < len) {
-            if (value === refFrom[idx]) {
-                return refTo[idx];
-            }
-            idx += 1;
-        }
-        refFrom[idx] = value;
-        refTo[idx] = copiedValue;
-        for (const key in value) {
-            if (Object.prototype.hasOwnProperty.call(value, key)) {
-                copiedValue[key] = deep ?
-                    _clone(value[key], refFrom, refTo, true) :
-                    value[key];
-            }
-        }
-        return copiedValue;
-    };
-    switch (type(value)) {
-    case 'Object':
-        return copy(Object.create(Object.getPrototypeOf(value)));
-    case 'Array':
-        return copy([]);
-    case 'Date':
-        return new Date(value.valueOf());
-    case 'RegExp':
-        return _cloneRegExp(value);
-    case 'Int8Array':
-    case 'Uint8Array':
-    case 'Uint8ClampedArray':
-    case 'Int16Array':
-    case 'Uint16Array':
-    case 'Int32Array':
-    case 'Uint32Array':
-    case 'Float32Array':
-    case 'Float64Array':
-    case 'BigInt64Array':
-    case 'BigUint64Array':
-        return value.slice();
-    default:
-        return value;
-    }
-}
